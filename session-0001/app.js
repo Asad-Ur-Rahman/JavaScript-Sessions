6e2a92aba568565cb00d36c2aa748e4ebec88b75
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-08-26 21:43:49 +0500

    Initial commit

---
+++ session-0001/app.js
@@ -0,0 +1,15 @@
+// alert("Hello")
+// console.log("Hello world");console.log("JS");
+// console.log("Hello world")
+// console.log("JS")
+
+// alert(3 + 
+//     1 
+// +
+// 2)
+
+// alert("hello")
+// [1,2].forEach(alert);
+// alert("hello")[1,2].forEach(alert);
+
+/**/ 
