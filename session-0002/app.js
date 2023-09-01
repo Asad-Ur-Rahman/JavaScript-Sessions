2121c56b8587b24e9012488b0c87bda8ae42d187
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-01 21:21:53 +0500

    session 2 code

---
+++ session-0002/app.js
@@ -0,0 +1,49 @@
+// // "use strict";
+
+// // console.log("Helloo")
+
+// alert("JavaScript")
+
+// "use strict";
+
+// console.log("Helloo")
+
+
+// variables;
+
+// let name;
+
+// name = "Lenovo";
+
+// console.log(name)
+
+// let x = 25, y = 33, z = "Js";
+// let x = 25,
+//     y = 33,
+//     z = "Js";
+
+// let x = 25;
+// let y = 33;
+// let z = "Js";
+
+
+// let x 
+
+// x = "False";
+// x = false;
+
+// x = 5;
+
+// "use strict";
+
+// x = 5;
+// console.log(x)
+
+
+const gravitit_g = 9.8;
+
+gravitit_g = 5;
+
+
+const COLOR_RED;
+const COLOR_GREEN;
