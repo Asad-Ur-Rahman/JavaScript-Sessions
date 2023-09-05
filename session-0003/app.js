0f0ee8b9916a5560aefd51aa23aa95d583a7ac05
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-05 06:29:15 +0500

    session 2 code

---
+++ session-0003/app.js
@@ -0,0 +1,12 @@
+// Interactions 
+// 1) alert
+// 2) prompt
+// 3) confirm
+
+// alert("Hello");
+
+// let age = prompt("Enter your age: ", 15);
+// alert(`User's age is ${age}`);
+
+let visit = confirm("Are you willing to visit our site?")
+alert(`User's age is ${visit}`);
