75c713579f6df68e0b700eac5a8ed39455783216
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-16 21:27:31 +0500

    session 6 code

---
+++ session-0006/app.js
@@ -0,0 +1,132 @@
+// let x = 0;
+
+// while(x < 6){
+//     console.log(x)
+//     x++;
+// }
+
+// let x = 6;
+
+// while(x > 0 ){
+//     console.log(x)
+//     x--;
+// }
+
+// let x = 6;
+
+// while(x > 0 ) console.log(x--)
+
+// let y = 8;
+
+// do {
+//     console.log(y);
+//     y--;
+// } while (y > 8);
+
+// for (let i = 1; i <= 10; i+=2) {
+//     console.log(`3 x ${i} = ${3 * i}`);
+    
+// }
+// let i;
+
+// for (i = 1; i <= 10; i+=2) {
+//     console.log(`3 x ${i} = ${3 * i}`);
+    
+// }
+
+// let i = 1
+
+// for (; i <= 10; i+=2) {
+//     console.log(`3 x ${i} = ${3 * i}`);
+    
+// }
+// let i = 1
+
+// for (; i <= 10;) {
+
+//     console.log(`3 x ${i} = ${3 * i}`);
+//     i++;
+    
+// }
+// let i = 1
+
+// for (;;) {
+
+//     console.log(`3 x ${i} = ${3 * i}`);
+    
+// }
+
+
+// for (let i = 0; i < 20; i++) {
+//     if(i%2 ==0) continue
+//     console.log(i)  
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     if (i == 6) {
+//        break 
+//     }
+//     console.log(i);
+// }
+
+// let x = 3;
+
+// switch (x) {
+//     case 6:
+//         console.log("Match with 6")
+//         break;
+//     case 8:
+//         console.log("Match with 8")
+//         break;
+//     case 5:
+//         console.log("Match with 5")
+//         break;
+//     case 3:
+//         console.log("Match with 3")
+//         break;
+
+//     default:
+//         console.log("No match found");
+//         break;
+// }
+// let x = 8;
+
+// switch (x) {
+//     case 6:
+//         console.log("Match with 6")
+        
+//     case 8:
+//         console.log("Match with 8")
+   
+//     case 5:
+//         console.log("Match with 5")
+        
+//     case 3:
+//         console.log("Match with 3")
+        
+
+//     default:
+//         console.log("No match found");
+        
+// }
+
+let x = 6;
+
+switch (x) {
+    case "6":
+        console.log("Match with 6")
+        break;
+    case 8:
+        console.log("Match with 8")
+        break;
+    case 5:
+        console.log("Match with 5")
+        break;
+    case 3:
+        console.log("Match with 3")
+        break;
+
+    default:
+        console.log("No match found");
+        break;
+}
