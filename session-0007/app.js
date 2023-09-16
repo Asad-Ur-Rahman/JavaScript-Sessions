f5433d0d63f1e7cafc1d6eb0c026d523731b52b7
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-17 00:19:50 +0500

    class 7 code

---
+++ session-0007/app.js
@@ -0,0 +1,12 @@
+function greetings() {
+    // alert("Good Morning");
+    console.log("Good Morning");
+}
+
+greetings();
+greetings();
+console.log("Hello");
+greetings();
+greetings();
+greetings();
+greetings();
