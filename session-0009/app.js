4870134d655172aa977bfa214ba4951a8901773e
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-30 20:20:23 +0500

    session 9 code

---
+++ session-0009/app.js
@@ -0,0 +1,60 @@
+
+
+
+// function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+// }
+
+// function showOk() {
+//     alert("You agreed.");
+// }
+
+// function showCancel() {
+//     alert("You canceled the execution.");
+// }
+
+
+// ask("Do you agree?", showOk, showCancel);
+
+// ask(
+//     "Do you agree?", 
+//     function (){ alert("You agreed."); }, 
+//     function () { alert("You canceled the execution.");}
+//     )
+
+
+// let sum = (a,b) => a+b;
+
+
+// console.log(sum(4,5));
+
+
+// let greetings = () => alert("Hello World")
+
+
+// greetings()
+
+// let sum = (a, b) => {
+//     let result = a + b;
+//     return result
+// };
+
+
+// console.log(sum(4, 5));
+
+
+// ask("Do you agree?", showOk, showCancel);
+
+// ask(
+//     "Do you agree?",
+//     () => alert("You agreed."),
+//     () => alert("You canceled the execution.")
+// )
+
+
+// ask(
+//     "Do you agree?",
+//     function () { alert("You agreed."); },
+//     function () { alert("You canceled the execution."); }
+// )
