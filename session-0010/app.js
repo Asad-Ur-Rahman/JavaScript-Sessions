ceb4496074b9dd9a7186849eab2a0edce0e4ae1d
Asad Ur Rahman <Asad.UrRahman@outlook.com>
2023-09-30 20:20:41 +0500

    session 10 code

---
+++ session-0010/app.js
@@ -0,0 +1,17 @@
+// console.log("hello"); console.log("world");
+
+// console.log("hello")
+// console.log("world")
+
+
+// alert("Hello world")
+// [1,2].array.forEach(alert)
+
+function name(params) {
+    
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
